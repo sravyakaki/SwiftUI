2009fab7d2d74312ee9773b675ab5ceaee383e9f
Sravya Kaki <sravyakaki@Sravyas-MacBook-Pro.local>
2016-06-30 12:20:59 -0400

    t This is the initial commit :wq! :wq !

---
+++ GitTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by Sravya Kaki on 6/29/16.
+//  Copyright © 2016 Sravya Kaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
