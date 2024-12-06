6e42870e16bb6e8c88c870671ccadcf9404a1a16
SKaki <sravyakaki@Sravyas-Mac-mini.local>
2024-12-05 21:10:49 -0800

    Project 2 part 3 challenge solved

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -12,6 +12,10 @@ struct ContentView: View {
     @State private var correctAnswer = Int.random(in: 0...2)
     @State private var showingScore = false
     @State private var scoreTitle = ""
+    @State private var currentScore = 0
+    @State private var selectedCountry = ""
+    @State private var numOfQuestions = 1
+    @State private var showFinalScore = false
     
     var body: some View {
         ZStack {
@@ -42,11 +46,11 @@ struct ContentView: View {
                         
                     }
                 }
-            .frame(maxWidth: .infinity)
-            .padding(.vertical,20)
-            .background(.regularMaterial)
-            .clipShape(.rect(cornerRadius: 20))
-                Text("Score: ???")
+                .frame(maxWidth: .infinity)
+                .padding(.vertical,20)
+                .background(.regularMaterial)
+                .clipShape(.rect(cornerRadius: 20))
+                Text("Score: \(currentScore)")
                     .foregroundStyle(.white)
                     .font(.title.bold())
             }.padding()
@@ -55,21 +59,49 @@ struct ContentView: View {
         .alert(scoreTitle, isPresented: $showingScore) {
             Button("Continue", action: askQuestion)
         } message: {
-            Text("Your score is ???")
+            if(scoreTitle == "Wrong") {
+                Text("""
+Wrong! That's the flag of \(selectedCountry)     
+Your score is \(currentScore)
+""")
+            } else {
+                Text("Your score is \(currentScore)")
+            }
+            
+        }
+        .alert("Game Over", isPresented: $showFinalScore) {
+            Button("Restart Game", action: finalScoreAlert )
+        } message: {
+            Text("Your final score is \(currentScore)")
         }
     }
     func flagTapped(_ number: Int) {
         if(number == correctAnswer) {
             scoreTitle = "Correct"
+            currentScore += 1
         } else {
             scoreTitle = "Wrong"
+            currentScore -= 1
+            selectedCountry = countries[number]
         }
         showingScore = true
     }
     
     func askQuestion() {
+        if(numOfQuestions < 8) {
+            countries.shuffle()
+            correctAnswer = Int.random(in: 0...2)
+            numOfQuestions += 1
+        } else {
+            showFinalScore = true
+        }
+    }
+    
+    func finalScoreAlert() {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
+        numOfQuestions = 1
+        currentScore = 0
     }
     
 }
