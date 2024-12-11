32ce874ea220b008891748d8cf630b58423d4caa
SKaki <sravyakaki@Sravyas-Mac-mini.local>
2024-12-10 16:27:25 -0800

    challenge day 25

---
+++ Day25Challenge/Day25Challenge/Day25ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day25ChallengeApp.swift
+//  Day25Challenge
+//
+//  Created by Sravya Kaki on 2024-12-08.
+//
+
+import SwiftUI
+
+@main
+struct Day25ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
