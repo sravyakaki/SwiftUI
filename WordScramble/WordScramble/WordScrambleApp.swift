68a15c2ca663c08c7e02daa51183e90004916ecd
luv_dev <sravyakaki@users.noreply.github.com>
2024-12-19 09:49:50 -0800

    Project 5 challenge solved

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Sravya Kaki on 12/16/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
