4f1d457bbc57405c6291a3ac1be913af6755dced
luv_dev <sravyakaki@users.noreply.github.com>
2025-01-01 17:53:05 -0800

    Day 35 challenge

---
+++ Day35Challenge/Day35Challenge/Day35ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day35ChallengeApp.swift
+//  Day35Challenge
+//
+//  Created by Sravya Kaki on 12/27/24.
+//
+
+import SwiftUI
+
+@main
+struct Day35ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
