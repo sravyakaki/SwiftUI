ebbdadcf0aa9cbe9b86cefaf6ababe948e935eea
SKaki <sravyakaki@Sravyas-Mac-mini.local>
2025-05-09 15:54:10 -0700

    Account view

--- Appetizers/Appetizers/Alerts/Alert.swift
+++ Appetizers/Appetizers/Alerts/Alert.swift
@@ -16,6 +16,7 @@ struct AlertItem: Identifiable {
 }
 
 struct AlertContext {
+    //MARK :- Network Alerts
     static let invalidData      = AlertItem(title: Text("Server Error"),
                                               message: Text("The data received from the server was indalid. Please contact support."),
                                               dismissButton: .default(Text("Ok")))
@@ -30,4 +31,20 @@ struct AlertContext {
     static let unableToComplete = AlertItem(title: Text("Server Error"),
                                             message: Text("Unable to complete this reqtuest. Please check your internet connection and try again later"),
                                               dismissButton: .default(Text("Ok")))
+    
+    //MARK :- Account Alerts
+    
+    static let invalidForm      = AlertItem(title: Text("Invalid Form"),
+                                              message: Text("Please ensure all fields have been filled out"),
+                                              dismissButton: .default(Text("Ok")))
+    static let invalidEmail     = AlertItem(title: Text("Invalid Email"),
+                                            message: Text("Please ensure your email is correct"),
+                                              dismissButton: .default(Text("Ok")))
+    static let userSaveSuccess  = AlertItem(title: Text("Profile saved"),
+                                            message: Text("Your profile information was succesfully saved"),
+                                              dismissButton: .default(Text("Ok")))
+    static let invalidUserData  = AlertItem(title: Text("Profile error"),
+                                            message: Text("There was an error saving/retrieving your profile data"),
+                                              dismissButton: .default(Text("Ok")))
+    
 }
